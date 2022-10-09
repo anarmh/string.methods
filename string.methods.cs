0a917f9e6a5a870b1d8594d3c6b05e389c00a678
anarmh <anarmh@code.edu.az>
2022-10-09 06:32:12 +0400

    add string.methods

---
+++ string.methods.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //int a = 6;
+            //int b = a;
+            //b = 16;
+            //Console.WriteLine(a+" "+b);
+
+            //int[] numbs1 = { 1, 2, 3, 4, 5, 6, 7 };
+            //int[] numbs2 = numbs1;
+            //numbs2[0] = 77;
+            //Console.WriteLine(numbs1[0]+" "+numbs2[0]);
+
+
+            //int num = 20;
+            //Console.WriteLine(num);
+
+            //num += 10;
+            //ShowNum(num);
+
+            //Console.WriteLine(num);
+
+            //int num;
+            //ShowNum(out num);
+            //num += 10;
+            //Console.WriteLine(num);
+
+
+            //int num=100;
+            //ShowNum(ref num);
+            //num += 10;
+            //Console.WriteLine(num);
+
+
+            //string name1 = "Anar";
+            //string name2 = name1;
+            //name2 = "Tunar";
+            //Console.WriteLine(name1);
+            //Console.WriteLine(name2);
+
+            //string name1 = "Anar";
+            //CheckStr( ref name1);
+            //Console.WriteLine(name1);
+
+            //string name1;
+            //Checkstr(out name1);
+            //Console.WriteLine(name1);
+
+            //string str1 = "An";
+            //string str2 = "ar";
+            //string name = str1 + str2;
+            //Console.WriteLine(name);
+
+            //StringBuilder str = new StringBuilder();
+
+            //str.Append("An");
+            //str.Append("ar");
+            //Console.WriteLine("str");
+
+
+            //string name = "Anar";
+            //Console.WriteLine(name.Length);
+
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //string name = "Anar Tunar";
+            //name = string.Empty;
+            //Console.WriteLine(name.Trim());
+
+            //Console.WriteLine(name.StartsWith("A"));
+
+            //string name = " Tunar";
+            //Console.WriteLine(name.EndsWith("r"));
+
+
+            //string s1 = "Hello ";
+            //string s2 = (String)s1.Clone();
+            //Console.WriteLine(s1);
+            //Console.WriteLine(s2);
+
+            //string s1 = "hello";
+            //string s2 = "hello";
+            //string s3 = "csharp";
+            //string s4 = "mello";
+
+            //Console.WriteLine(string.Compare(s1, s2));
+            //Console.WriteLine(string.Compare(s2, s3));
+            //Console.WriteLine(string.Compare(s3, s4));
+
+            //string s1 = "Hello ";
+            //string s2 = "He";
+            //string s3 = "Hi";
+            //Console.WriteLine(s1.Contains(s2));
+            //Console.WriteLine(s1.Contains(s3));
+
+            //string s1 = "Hello";
+            //string s2 = "Hello";
+            //string s3 = "Bye";
+            //Console.WriteLine(s1.Equals(s2));
+            //Console.WriteLine(s1.Equals(s3));
+
+            //string s1 = "Hello C#";
+            //Console.WriteLine(s1.GetType());
+
+            //string s1 = "Hello C#";
+            //Console.WriteLine(s1.GetTypeCode());
+        }
+
+        //public static void ShowNum(out int num)
+        //{
+        //    num = 100;
+        //    num += 10;
+        //    Console.WriteLine(num);
+        //}
+        //public static void CheckStr( ref string str)
+        //{
+        //    str = "Tunar";
+        //    Console.WriteLine(str);
+        //}
+
+        //public static void Checkstr(out string str)
+        //{
+        //    str = "Tunar";
+        //    str = "Anar";
+        //    Console.WriteLine(str);
+        //}
+
+          
+
+    }
+}
